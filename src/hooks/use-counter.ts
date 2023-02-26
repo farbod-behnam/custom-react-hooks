43f5c71b07d80666272dc4dd2054e3273b035553
farbod-behnam <farbod.behnam@gmail.com>
2023-02-26 12:50:14 +0330

    configure useCounter custom hooks

--- src/hooks/use-counter.ts
+++ src/hooks/use-counter.ts
@@ -1,15 +1,22 @@
 import { useEffect, useState } from "react";
 
-export default function useCounter() {
-    const [counter, setCounter] = useState<number>(0);
 
-    useEffect(() => {
-      const interval = setInterval(() => {
+
+export default function useCounter(isForward = true) {
+  const [counter, setCounter] = useState<number>(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (isForward) {
         setCounter((prevCounter) => prevCounter + 1);
-      }, 1000);
-  
-      return () => clearInterval(interval);
-    }, []);
+      }
+      else {
+        setCounter((prevCounter) => prevCounter - 1);
+      }
+    }, 1000);
 
-    return counter;
+    return () => clearInterval(interval);
+  }, [isForward]);
+
+  return counter;
 }
